1dfc22d3c7cbbc0c1455b758416f3190ef4cade1
eslamghoniem <eslamghoniem@users.noreply.github.com>
2018-06-04 20:36:39 +0200

    filter
    filterbybrand

--- Gearstore/Controllers/ClientProductsController.cs
+++ Gearstore/Controllers/ClientProductsController.cs
@@ -13,12 +13,12 @@ namespace gearproj.Controllers
     public class ClientProductsController : ApiController
     {
         ApplicationDbContext db = new ApplicationDbContext();
-        
-        public IHttpActionResult Get(int pagenum)
+        [HttpGet, Route("api/ClientProducts")]
+        public IHttpActionResult Get(int pagenum ,string name=null)
         {
          int pgn = pagenum < 0 ? 1 :  pagenum > Math.Ceiling(db.products.Count() / 8.0) ?  (int)Math.Ceiling(db.products.Count() / 8.0) : pagenum ;
             int count = db.products.Count() < pgn*8 ? ((pgn-1) * 8 )  : (pgn-1)*8 ;
-            var prods = db.products.OrderByDescending(k => k.productId).Skip(count).Take(8).ToList();
+            var prods = db.products.Where(a=>a.ProductName==name||name==null).OrderByDescending(k => k.productId).Skip(count).Take(8).ToList();
             if (prods == null)
             {
                 return BadRequest();
@@ -26,7 +26,24 @@ namespace gearproj.Controllers
             else
              return Ok(prods);
         }
-        
+        [HttpGet, Route("api/filterClientProducts")]
+        public IHttpActionResult Get(int pagenum,string catename=" ",string  brandsname=" ")
+        {
+            string[] brands = brandsname.Split(',');
+            string[] catnames = catename.Split(',');
+            var brandsid = db.Brands.Where(a => brands.Contains(a.BrandName)).Select(a => a.BrandId).ToList();
+            var cats = db.Categories.Where(a => catnames.Contains(a.CategoriesName)).Select(a=>a.CategoriesId).ToList();
+            int pgn = pagenum < 0 ? 1 : pagenum > Math.Ceiling(db.products.Count() / 8.0) ? (int)Math.Ceiling(db.products.Count() / 8.0) : pagenum;
+            int count = db.products.Count() < pgn * 8 ? ((pgn - 1) * 8) : (pgn - 1) * 8;
+            var prods = db.products.Where(a=>cats.Contains(a.CategoryId)|| brandsid.Contains(a.BrandId)).OrderByDescending(k => k.productId).Skip(count).Take(8).ToList();
+            if (prods == null)
+            {
+                return BadRequest();
+            }
+            else
+                return Ok(prods);
+        }
+
 
         //get top selling products
 
