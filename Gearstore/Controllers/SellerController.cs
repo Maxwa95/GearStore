8637308f8e28b38fbe015fea564137f5396da71c
hodaahmed <hodaahmed@users.noreply.github.com>
2018-06-05 13:37:25 +0200

    modifications

--- Gearstore/Controllers/SellerController.cs
+++ Gearstore/Controllers/SellerController.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace gearproj.Controllers
@@ -44,15 +46,86 @@ namespace gearproj.Controllers
                 return BadRequest();
         }
         
-        // POST: api/Seller
-        [Authorize(Roles = "Seller")]
+        //// POST: api/Seller
+        //[Authorize(Roles = "Seller")]
+        //[HttpPost, Route("api/seller/product")]
+        //public IHttpActionResult Post([FromBody]Product product)
+        //{
+           
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.products.Add(product);
+        //        db.SaveChanges();
+        //        return Ok(db.products.ToList());
+        //    }
+        //    else
+        //        return BadRequest();
+
+        //}
+       
         [HttpPost, Route("api/seller/product")]
         public IHttpActionResult Post([FromBody]Product product)
         {
-           
+
             if (ModelState.IsValid)
             {
-                db.products.Add(product);
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                try
+                { 
+                var httpRequest = HttpContext.Current.Request;
+
+                foreach (string file in httpRequest.Files)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+                    var postedFile = httpRequest.Files[file];
+                    if (postedFile != null && postedFile.ContentLength > 0)
+                    {
+
+                        int MaxContentLength = 1024 * 1024 * 1; //Size = 1 MB  
+
+                        IList<string> AllowedFileExtensions = new List<string> { ".jpg", ".gif", ".png" };
+                        var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+                        var extension = ext.ToLower();
+                        if (!AllowedFileExtensions.Contains(extension))
+                        {
+
+                            var message = string.Format("Please Upload image of type .jpg,.gif,.png.");
+                                
+                            return BadRequest(message);
+                        }
+                        else if (postedFile.ContentLength > MaxContentLength)
+                        {
+
+                            var message = string.Format("Please Upload a file upto 1 mb.");
+                                
+                            return BadRequest(message);
+                        }
+                        else
+                        {
+
+                            var filePath = HttpContext.Current.Server.MapPath("~/Userimage/" + postedFile.FileName + extension);
+
+                            postedFile.SaveAs(filePath);
+
+                        }
+                    }
+
+                    var message1 = string.Format("Image Updated Successfully.");
+                    return Ok(message1); ;
+                }
+                var res = string.Format("Please Upload a image.");
+                    return BadRequest(res);
+                 }
+            catch (Exception ex)
+            {
+                var res = string.Format("some Message");
+                return NotFound();
+            }
+        
+
+
+        db.products.Add(product);
                 db.SaveChanges();
                 return Ok(db.products.ToList());
             }
@@ -60,6 +133,7 @@ namespace gearproj.Controllers
                 return BadRequest();
 
         }
+
 
         // PUT: api/Seller/5
         [Authorize(Roles = "Seller")]
